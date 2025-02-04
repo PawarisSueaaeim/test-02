00107d2c5517ee724d7f27c805596ae41cc8b90c
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-04 15:27:16 +0700

    refactor: clean up

--- app/auto-delete-todo-list-v1/page.tsx
+++ app/auto-delete-todo-list-v1/page.tsx
@@ -8,9 +8,6 @@ const baseUrl = process.env.NEXT_PUBLIC_API;
 export default async function AutoDeleteTodoListV1({}: Props) {
     const response = await fetch(`${baseUrl}/todo-list`);
     const listDatas = await response.json();
-    console.log(typeof listDatas);
-    console.log(typeof response);
-    console.log(listDatas);
 
     return (
         <div>
