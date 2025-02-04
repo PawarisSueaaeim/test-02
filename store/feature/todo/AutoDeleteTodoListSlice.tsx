190fbea6eb36dfffacf20871e7076a56f9a38f34
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-04 16:57:31 +0700

    fix: click move back

--- store/feature/todo/AutoDeleteTodoListSlice.tsx
+++ store/feature/todo/AutoDeleteTodoListSlice.tsx
@@ -8,10 +8,10 @@ export type ITodoList = {
         name: string;
         type: "Fruit" | "Vegetable" | "None";
         status: "Fruit" | "Vegetable" | "None";
-    }[],
+    }[];
     onDragActiveCard: string;
     onDragActiveType: string;
-}
+};
 
 const initialState: ITodoList = {
     listDatas: [],
@@ -24,7 +24,7 @@ export const autoDeleteTodoListSlice = createSlice({
     initialState: initialState,
     reducers: {
         setState: (state, action) => {
-            const {value, keyValue} = action.payload;
+            const { value, keyValue } = action.payload;
             switch (keyValue) {
                 case "listDatas": {
                     state.listDatas = processListDatas(value);
@@ -42,14 +42,16 @@ export const autoDeleteTodoListSlice = createSlice({
         },
         setStatus: (state, action) => {
             const { id, value } = action.payload;
-            state.listDatas.find((item) => item.id === id)!.status = value;
-        }
+            const item = state.listDatas.find((item) => item.id === id);
+            if (item && item.status !== "None") {
+                item.status = "None";
+            } else if (item) {
+                item.status = value;
+            }
+        },
     },
 });
 
-export const {
-    setState,
-    setStatus
-} = autoDeleteTodoListSlice.actions;
+export const { setState, setStatus } = autoDeleteTodoListSlice.actions;
 export default autoDeleteTodoListSlice.reducer;
 export const autoDeleteTodoListSelector = (state: RootStore) => state.autoDeleteTodoListSlice;
