1956ccfdfb4545a1b55eeb51752e7aad53650751
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2025-02-06 16:15:34 +0700

    fix: todo list v2

--- components/todo/TodoV2.tsx
+++ components/todo/TodoV2.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { setState, setStatus } from "@/store/feature/todo/AutoDeleteTodoListSlice";
+import { setState } from "@/store/feature/todo/AutoDeleteTodoListSlice";
 import { RootStore } from "@/store/store";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Column from "./Column";
 import { IListData } from "./TodoV1";
@@ -13,23 +13,10 @@ type Props = {
 export default function TodoV2({ datas }: Props) {
     const dispatch = useDispatch();
     const { listDatas } = useSelector((state: RootStore) => state.autoDeleteTodoListSlice);
-    const timeoutRefs = useRef<{ [key: string]: NodeJS.Timeout }>({});
 
     useEffect(() => {
         dispatch(setState({ value: datas, keyValue: "listDatas" }));
     }, [datas]);
-
-    useEffect(() => {
-        listDatas.map((item) => {
-            if (item.status !== "None") {
-                timeoutRefs.current[item.id] = setTimeout(() => {
-                    dispatch(setStatus({ id: item.id, value: "None" }));
-                }, 5000);
-            } else if (timeoutRefs.current[item.id]) {
-                clearTimeout(timeoutRefs.current[item.id]);
-            }
-        });
-    }, [listDatas]);
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
